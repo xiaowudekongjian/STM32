670f1454b44b5523ff25fb8e261cf76081a797f6
羊咩 <212190935@qq.com>
2025-06-28 18:14:59 +0800

    版本1.1.01

---
+++ User/Inc/Unicode_gbk.h
@@ -0,0 +1,17 @@
+#ifndef __UNICODE_GBK_H__
+#define __UNICODE_GBK_H__
+
+#include  "main.h"
+#define MAX_UNI_INDEX  7768
+
+extern const uint16_t GB_TO_UNI[MAX_UNI_INDEX][2];
+
+
+
+
+
+
+
+#endif
+
+
